c2eb4446843c77ade1f3885eea55d3e55398083d
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-09-25 14:05:22 -0700

    Initial commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,12 +11,12 @@ def safe_print_list(my_list=[], x=0):
         int: The count of elements that were successfully printed.
     """
 
-    cout = 0
+    count = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i], end=""))
-            cout += 1
-            except IndexError:
-                break
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
     print("")
     return (count)
