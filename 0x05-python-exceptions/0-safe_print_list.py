a36d8bec25a50f33bdd3c45f0d1c6b59ac5bc5ad
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-09-25 14:00:26 -0700

    Initial commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    """Print x number of elements from a list.
+
+    Parameters:
+        my_list (list): The list containing elements to be printed.
+        x (int): The number of elements from my_list to be printed.
+
+    Returns:
+        int: The count of elements that were successfully printed.
+    """
+
     cout = 0
     for i in range(x):
         try:
