f97bfedaff04c2b2ba4983918cea5768f3f2f5a9
Hermas Naalebkuur <naalebkuurh@gmail.com>
2023-10-30 16:44:11 -0700

    last commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Representing a base geometry."""
+
+    def area(self):
+        """if not implemented."""
+        raise Exception("area() is not implemented")
