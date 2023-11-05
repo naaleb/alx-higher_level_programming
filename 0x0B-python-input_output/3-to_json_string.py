36026d8290ae96f1505aa1dd8b46d6ac8e8384b7
Hermas Naalebkuur <53911569+naaleb@users.noreply.github.com>
2023-11-05 05:28:11 +0000

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Defines an object to JSON function."""
-import json
-
-
-def to_json_string(my_obj):
-    """Return the JSON representation of an object."""
-    return json.dumps(my_obj)
--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Defining a Python class to JSON function."""
-
-
-def class_to_json(obj):
-    """Return the dictionary description of a simple data structure."""
-    return obj.__dict__
